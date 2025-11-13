2e632b6b4bbaebe5060fbee23eb761e012d79d57
HerreraHernandezJovannaJatziry <20223tn062@utez.edu.mx>
2025-11-12 18:55:33 -0600

    [ADD] Power ups

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public int damage = 1;
+    public float lifetime = 3f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * (speed * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
