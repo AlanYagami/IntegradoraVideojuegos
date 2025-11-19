65887818b7737d9a3041e7259206575603beb82c
Leyla-Villanueva <leylavillanueva0205@gmail.com>
2025-11-19 12:36:14 -0600

    [ADD] Menu seleccionador de naves

---
+++ Assets/Scripts/Personajes.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPersonaje", menuName = "Personaje")]
+public class Personajes : ScriptableObject
+{
+    public GameObject personajeJugable;
+    public Sprite imagen;
+    public string nombre;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
