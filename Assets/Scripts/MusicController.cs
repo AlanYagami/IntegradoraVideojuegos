53479264833e4af11d09f297720982b80f569d56
DiegoJesusP <spartfomi@outlook.com>
2025-12-05 10:16:24 -0600

    [FEATURE] se agrego la funcionalidad de poder seleccionar la nave del player y poder ocuparla en el juego

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicaFondo;
+
+    public void PlayMusic()
+    {
+        musicSource.PlayOneShot(musicaFondo);
+    }
+}
